3718dc65883ee4dcd045895e4159b68037882b6e
ellisonv <gm@ellisonventures.com>
2012-11-08 19:31:23 -0800

    working through basic logistics

---
+++ conf/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  
+  'event-granularity': 60000,
+  'event-expiration': 604800
+  
+}
---
+++ index.js
@@ -0,0 +1,21 @@
+var conf = require('./conf/config');
+module.exports = function(a, b) {
+  
+  typeof a==='object' ? r = a : r = require('redis').createClient(a, b);
+  
+  var interface = function(event, onReady) {
+    var now = new Date().getTime();
+    var hash = 'hit_'+(now-(now%conf['event-granularity'])).toString();
+    r.hincrby(hash, event, 1, function(err, res) {
+      if (res===1) {
+         
+      }
+    });
+  }
+  
+  interface.count = function(event, start, stop) {
+    console.log(event)
+  }
+  return interface;
+  
+}
